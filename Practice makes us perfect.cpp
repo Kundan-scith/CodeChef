5bf7564f0c0c6cd187f65924c5f98060b0eb56b7
Kundan Kumar <85442986+Kundan-scith@users.noreply.github.com>
2022-10-30 11:39:58 +0530

    Create Practice makes us perfect.cpp

---
+++ Practice makes us perfect.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int arr[4];
+	int k=0;
+	for(int i=0;i<4;i++){
+	    cin>>arr[i];
+	    if(arr[i]>=10){
+	     k++;   
+	    }}
+	cout<<k;
+	return 0;
+}
